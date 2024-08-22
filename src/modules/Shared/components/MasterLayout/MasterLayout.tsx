f2ab898584743e06b0aa2ade17a34a4b337ad188
OsamaElgendy <114953561+OsamaElgndy@users.noreply.github.com>
2024-08-22 04:03:43 +0300

    Add files via upload

--- src/modules/Shared/components/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/components/MasterLayout/MasterLayout.tsx
@@ -1,20 +1,16 @@
 import { Outlet } from "react-router-dom";
 import UsePageTitle from "../../../../utils/UsePageTitle";
 import SideBar from "../SideBar/SideBar";
-import LayoutHeader from "../LayoutHeader/LayoutHeader";
 import "./MasterLayout.scss";
 
 const MasterLayout = () => {
     UsePageTitle();
 
   return (
-    <div id="master_bx">
-      <LayoutHeader />
-      <div className="content-bx">
-        <SideBar />
-        <Outlet />
-      </div>
-    </div>
+    <>
+      <SideBar />
+      <Outlet />
+    </>
   );
 };
 
