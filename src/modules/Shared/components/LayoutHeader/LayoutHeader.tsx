8220cb6e6667c9aed8ef7300d7c8ffa24f4b7497
Mohammmed sayrafi <127789987+Mohammedsyr98@users.noreply.github.com>
2024-08-26 22:18:10 +0300

    Update Layout Header Component - Make it responsive - get CurrentUser

--- src/modules/Shared/components/LayoutHeader/LayoutHeader.tsx
+++ src/modules/Shared/components/LayoutHeader/LayoutHeader.tsx
@@ -4,10 +4,48 @@ import { BASE_IMG_URL } from "../../../../constants/END_POINTS";
 import userAvatar from "../../../../assets/images/userAvatar.png";
 import { Dropdown, DropdownButton } from "react-bootstrap";
 import useUserInformation from "../../../../constants/useUserInformation";
+import useScreenSize from "../../../../constants/useScreenSize";
+import { setToken } from "../../../../constants/Tokenhandler";
+import { useNavigate } from "react-router-dom";
+
+interface UserInforamtion {
+  userName: string;
+  email: string;
+  imagePath: string | null;
+}
+
+const UserDetails = ({
+  userInformation,
+}: {
+  userInformation: UserInforamtion;
+}) => {
+  const { userName, email, imagePath } = userInformation;
+
+  return (
+    <div className="user-details d-flex flex-row align-items-center">
+      <img
+        className="user-avatar"
+        src={imagePath ? `${BASE_IMG_URL}/${imagePath}` : userAvatar}
+        alt={`${userName}'s avatar`}
+      />
+
+      <div className="user-credentials d-flex flex-column">
+        <span className="user-name">{userName}</span>
+        <span className="user-email">{email}</span>
+      </div>
+    </div>
+  );
+};
 
 const LayoutHeader = () => {
   const { userInformation } = useUserInformation();
+  const { screenSizeCategory } = useScreenSize();
+  const navigate = useNavigate();
 
+  const logOut = () => {
+    setToken("");
+    navigate("/login");
+  };
   return (
     <div id="layout_header">
       <img src={IMAGES.headerLogo} alt="pic" />
@@ -15,23 +53,21 @@ const LayoutHeader = () => {
         <div className="user-info-container d-flex flex-row align-items-center">
           <i className="notification-icon fa-solid fa-bell" />
 
-          <div className="user-details d-flex flex-row align-items-center">
-            <img
-              className="user-avatar"
-              src={
-                userInformation?.imagePath
-                  ? `${BASE_IMG_URL}/${userInformation.imagePath}`
-                  : userAvatar
-              }
-            />
-            <div className="user-credentials d-flex flex-column">
-              <span className="user-name">{userInformation.userName}</span>
-              <span className="user-email">{userInformation.email}</span>
-            </div>
+          {screenSizeCategory === "large" ? (
+            <>
+              <UserDetails userInformation={userInformation} />
+              <DropdownButton
+                title={<i className="fa-solid fa-chevron-down" />}>
+                {" "}
+                <Dropdown.Item onClick={logOut}>Log Out</Dropdown.Item>
+              </DropdownButton>
+            </>
+          ) : (
             <DropdownButton title={<i className="fa-solid fa-chevron-down" />}>
-              <Dropdown.Item href="#/action-1">Log Out</Dropdown.Item>
+              <UserDetails userInformation={userInformation} />
+              <Dropdown.Item onClick={logOut}>Log Out</Dropdown.Item>
             </DropdownButton>
-          </div>
+          )}
         </div>
       )}
     </div>
