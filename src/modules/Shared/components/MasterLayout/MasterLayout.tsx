c753d86b74fe7ccfbf0743605f01194a9e64c6fc
mahmoudhamed169 <mh4190207@gmail.com>
2024-08-27 23:30:36 +0300

    refactor MasterLayout component

--- src/modules/Shared/components/MasterLayout/MasterLayout.tsx
+++ src/modules/Shared/components/MasterLayout/MasterLayout.tsx
@@ -2,14 +2,34 @@ import { Outlet } from "react-router-dom";
 import UsePageTitle from "../../../../utils/UsePageTitle";
 import SideBar from "../SideBar/SideBar";
 import "./MasterLayout.scss";
+import Navbar from "../Navbar/Navbar";
+
+import { useState } from "react";
 
 const MasterLayout = () => {
-    UsePageTitle();
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  UsePageTitle();
+
+  const toggleSidebar = (): void => {
+    setCollapsed(!collapsed);
+  };
 
   return (
     <>
-      <SideBar />
-      <Outlet />
+      <div className="vh-100 ">
+        <div className="col-12 ">
+          <Navbar />
+        </div>
+
+        <div className={`d-flex ${collapsed ? "collapsed" : ""}   `}>
+          <div className="sideBar-container position-fixed ">
+            <SideBar toggleSidebar={toggleSidebar} />
+          </div>
+          <div className="content ">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
