855ef1ef06d26f7fb21b8d7bc80a0a39d7c15f05
Mohammmed sayrafi <127789987+Mohammedsyr98@users.noreply.github.com>
2024-08-29 17:46:19 +0300

    Update ProjectList.tsx

--- src/modules/Projects/components/ProjectsList/ProjectsList.tsx
+++ src/modules/Projects/components/ProjectsList/ProjectsList.tsx
@@ -1,9 +1,14 @@
-import './ProjectsList.scss';
+import "./ProjectsList.scss";
 
 const ProjectsList = () => {
   return (
-    <div>ProjectsList</div>
-  )
-}
+    <div id="projects-list">
+      <div className="head">
+        <span className="title">Projects</span>
+        <button>+ Add New Project</button>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectsList
+export default ProjectsList;
