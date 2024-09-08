59e0777707f2adbbf23dcd7371df25ea8f5ce46b
mahmoudhamed169 <mh4190207@gmail.com>
2024-09-08 22:26:14 +0300

    salove conf

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ChangePass from "./modules/Auth/components/ChangePass/ChangePass";
+import ForgetPass from "./modules/Auth/components/ForgetPass/ForgetPass";
+import Login from "./modules/Auth/components/Login/Login";
+import Register from "./modules/Auth/components/Register/Register";
+import ResetPass from "./modules/Auth/components/ResetPass/ResetPass";
+import VerifyAccount from "./modules/Auth/components/VerifyAccount/VerifyAccount";
+import Dashboard from "./modules/Dashboard/components/Dashboard";
+import ProjectData from "./modules/Projects/components/ProjectData/ProjectData";
+import ProjectsList from "./modules/Projects/components/ProjectsList/ProjectsList";
 import AuthLayout from "./modules/Shared/components/AuthLayout/AuthLayout";
 import MasterLayout from "./modules/Shared/components/MasterLayout/MasterLayout";
 import NotFound from "./modules/Shared/components/NotFound/NotFound";
-import Login from "./modules/Auth/components/Login/Login";
-import Register from "./modules/Auth/components/Register/Register";
-import ChangePass from "./modules/Auth/components/ChangePass/ChangePass";
-import VerifyAccount from "./modules/Auth/components/VerifyAccount/VerifyAccount";
-import ForgetPass from "./modules/Auth/components/ForgetPass/ForgetPass";
-import ResetPass from "./modules/Auth/components/ResetPass/ResetPass";
-import Dashboard from "./modules/Dashboard/components/Dashboard";
-import ProjectsList from "./modules/Projects/components/ProjectsList/ProjectsList";
-import ProjectData from "./modules/Projects/components/ProjectData/ProjectData";
-import TasksList from "./modules/Tasks/components/TasksList/TasksList";
 import TaskData from "./modules/Tasks/components/TaskData/TaskData";
+import TasksList from "./modules/Tasks/components/TasksList/TasksList";
 import UsersList from "./modules/Users/components/UsersList/UsersList";
 
 //toastify
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AddProject from "./modules/Projects/components/AddProject.tsx/AddProject";
+import UpdateProject from "./modules/Projects/components/UpdateProject/UpdateProject";
 import { ProtectedRoute } from "./modules/Shared/components/ProtectedRoute/ProtectedRoute";
 
 const App = () => {
@@ -47,12 +49,20 @@ const App = () => {
       children: [
         { index: true, element: <Dashboard /> },
         { path: "", element: <Dashboard /> },
-        { path: "projects", element: <ProjectsList /> },
+        {
+          path: "projects",
+          element: <ProjectsList />,
+        },
         { path: "project-data", element: <ProjectData /> },
-        { path: "tasks", element: <TasksList /> },
-        { path: "tasks-data", element: <TaskData /> },
+        {
+          path: "tasks",
+          element: <TasksList />,
+        },
+        { path: "add-task", element: <TaskData /> },
+        { path: "update-task/:id", element: <TaskData /> },
         { path: "users", element: <UsersList /> },
-
+        { path: "add-project", element: <AddProject /> },
+        { path: "update-project", element: <UpdateProject /> },
       ],
     },
   ]);
